5558f8480d7e5b03761dbb9fbb92725a2bf86e3a
Heather Escoto <heather.escoto3@gmail.com>
2023-02-03 12:47:05 -0500

    Initial Commit

---
+++ Challenge5.6/Challenge5_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge5_6App.swift
+//  Challenge5.6
+//
+//  Created by Heather Escoto on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenge5_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
